132084ea1b9e078f6f900db5712ae4eb71dca2de
Christer Olsen <colsen1991@gmail.com>
2018-03-14 11:21:46 +0100

    Changed 'meep' for 'Vue'

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 export default {
-  install (meep) {
-    meep.prototype.$createSrc = function (baseSrc, w = null, auto = 'format', fm = 'jpg', fit = 'max', q = 45) {
+  install (Vue) {
+    Vue.prototype.$createSrc = function (baseSrc, w = null, auto = 'format', fm = 'jpg', fit = 'max', q = 45) {
       return `${baseSrc}?auto=${auto}&fm=${fm}&q=${q}&fit=${fit}${w ? `&w=${w}` : ''}`
     }
 
-    meep.prototype.$createSizes = function (standard = '50vw', isMobile = false) {
+    Vue.prototype.$createSizes = function (standard = '50vw', isMobile = false) {
       return `(max-width: 768px) ${isMobile ? '100vw' : standard}, ${standard}`
     }
 
-    meep.prototype.$createSrcSet = function (src, from = 400, to = 1400, step = 100) {
+    Vue.prototype.$createSrcSet = function (src, from = 400, to = 1400, step = 100) {
       let srcSet = ''
       let firstSrcDone = false
 
