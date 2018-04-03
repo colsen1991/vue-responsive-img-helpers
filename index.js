2b5c0408facd987fa0bed6a724fff381aa881125
Christer Olsen <colsen1991@gmail.com>
2018-04-03 11:15:59 +0200

    babel

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
+'use strict';
+
 module.exports = {
-  install (Vue) {
-    Vue.prototype.$createSrc = function (baseSrc, w = null, auto = 'format', fm = 'jpg', fit = 'max', q = 45) {
-      return `${baseSrc}?auto=${auto}&fm=${fm}&q=${q}&fit=${fit}${w ? `&w=${w}` : ''}`
-    }
+  install: function install(Vue) {
+    Vue.prototype.$createSrc = function (baseSrc) {
+      var w = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+      var auto = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'format';
+      var fm = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 'jpg';
+      var fit = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 'max';
+      var q = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : 45;
 
-    Vue.prototype.$createSizes = function (standard = '50vw', isMobile = false) {
-      return `(max-width: 768px) ${isMobile ? '100vw' : standard}, ${standard}`
-    }
+      return baseSrc + '?auto=' + auto + '&fm=' + fm + '&q=' + q + '&fit=' + fit + (w ? '&w=' + w : '');
+    };
 
-    Vue.prototype.$createSrcSet = function (src, from = 400, to = 1400, step = 100) {
-      let srcSet = ''
-      let firstSrcDone = false
+    Vue.prototype.$createSizes = function () {
+      var standard = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '50vw';
+      var isMobile = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
 
-      for (let i = from; i <= to; i += step) {
-        srcSet += `${firstSrcDone ? ', ' : ''}${src}&w=${i} ${i}w`
+      return '(max-width: 768px) ' + (isMobile ? '100vw' : standard) + ', ' + standard;
+    };
 
-        if (!firstSrcDone) firstSrcDone = true
+    Vue.prototype.$createSrcSet = function (src) {
+      var from = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 400;
+      var to = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1400;
+      var step = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 100;
+
+      var srcSet = '';
+      var firstSrcDone = false;
+
+      for (var i = from; i <= to; i += step) {
+        srcSet += '' + (firstSrcDone ? ', ' : '') + src + '&w=' + i + ' ' + i + 'w';
+
+        if (!firstSrcDone) firstSrcDone = true;
       }
 
-      return srcSet
-    }
+      return srcSet;
+    };
   }
-}
+};
---
+++ src/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  install (Vue) {
+    Vue.prototype.$createSrc = function (baseSrc, w = null, auto = 'format', fm = 'jpg', fit = 'max', q = 45) {
+      return `${baseSrc}?auto=${auto}&fm=${fm}&q=${q}&fit=${fit}${w ? `&w=${w}` : ''}`
+    }
+
+    Vue.prototype.$createSizes = function (standard = '50vw', isMobile = false) {
+      return `(max-width: 768px) ${isMobile ? '100vw' : standard}, ${standard}`
+    }
+
+    Vue.prototype.$createSrcSet = function (src, from = 400, to = 1400, step = 100) {
+      let srcSet = ''
+      let firstSrcDone = false
+
+      for (let i = from; i <= to; i += step) {
+        srcSet += `${firstSrcDone ? ', ' : ''}${src}&w=${i} ${i}w`
+
+        if (!firstSrcDone) firstSrcDone = true
+      }
+
+      return srcSet
+    }
+  }
+}
