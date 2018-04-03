ce6679dbbe3aedcd1a7b7d0d885b2398f1ba4e5e
Christer Olsen <colsen1991@gmail.com>
2018-04-03 14:14:42 +0200

    Move away from auto=format. Causing to much trouble atm and fallback is not working correctly it seems.

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ module.exports = {
   install: function install(Vue) {
     Vue.prototype.$createSrc = function (baseSrc) {
       var w = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
-      var fm = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'jpg';
-      var auto = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 'format';
+      var q = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+      var auto = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 'compress';
       var fit = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 'max';
-      var q = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : 35;
 
-      return baseSrc + '?auto=' + auto + '&fm=' + fm + '&q=' + q + '&fit=' + fit + (w ? '&w=' + w : '');
+      return baseSrc + '?auto=' + auto + '&q=' + q + '&fit=' + fit + (w ? '&w=' + w : '') + (q ? '&q=' + q : '');
     };
 
     Vue.prototype.$createSizes = function () {
